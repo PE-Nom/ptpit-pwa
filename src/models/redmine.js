267097e625e272ce6348fa7050dc31229c73cc2c
PE-Nom <ib65629@gmail.com>
2018-09-27 23:40:16 +0900

    AWSにアップ

--- src/models/redmine.js
+++ src/models/redmine.js
@@ -7,21 +7,21 @@ import axios from 'axios'
 // const BASE_URL = 'http://nomsan-elb-2142077815.ap-northeast-1.elb.amazonaws.com/redmine' // @AWS
 // -------
 // const BASE_URL = 'https://192.168.1.4/redmine' // @office on dell over https
-const BASE_URL = 'https://192.168.10.6/redmine' // @home on dell over https
+// const BASE_URL = 'https://192.168.10.6/redmine' // @home on dell over https
 // const BASE_URL = 'https://192.168.10.5/redmine' // @home on Let's-note over https
 // const BASE_URL = 'https://nomsan-elb-2142077815.ap-northeast-1.elb.amazonaws.com/redmine' // @AWS
 
 // const BASE_URL = 'https://www.nomtech-pwa.com/redmine' // @AWS
-// const BASE_URL = 'https://pitsan.nomtech-pwa.com/redmine' // @AWS
+const BASE_URL = 'https://pitsan.nomtech-pwa.com/redmine' // @AWS
 
 export default {
   rmc: null,
   configured: false,
   user: {
-    username: 'mhims1',
-    password: 'mhims0821'
-    // username: 'patroler1',
-    // password: 'safety0918'
+    // username: 'mhims1',
+    // password: 'mhims0821'
+    username: 'patroler1',
+    password: 'safety0918'
   },
   configure () {
     if (!this.isConfigured()) {
